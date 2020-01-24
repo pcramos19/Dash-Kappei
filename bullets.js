8fde484e5e59ab17da2c912bfb30fa569f50ee7f
Teo <teodorolg130@gmail.com>
2020-01-24 10:47:47 +0100

    first draft

---
+++ bullets.js
@@ -0,0 +1,35 @@
+class Bullets {
+  constructor(ctx, playerPosX, playerPosY, playerPosY0, playerWidth, playerHeight) {
+    this.ctx = ctx;
+    this.posX = playerPosX + playerWidth;
+    this.posY = playerPosY + playerHeight / 2;
+    this.playerPosY0 = playerPosY0;
+    this.playerHeight = playerHeight;
+
+    this.radius = 5;
+
+    this.velX = 10;
+    this.velY = 1;
+
+    this.gravity = 1;
+  }
+
+  draw() {
+    this.ctx.beginPath();
+    this.ctx.fillStyle = "whitesmoke";
+    this.ctx.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2);
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+
+  move() {
+    this.posX += this.velX;
+    this.posY += this.velY;
+
+    this.velY += this.gravity;
+
+    if (this.posY >= this.playerPosY0 + this.playerHeight) {
+      this.velY *= -1;
+    }
+  }
+}
